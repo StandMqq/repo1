34bb3b3acda76de1960846f63c0d6f409273dcee
LegendaryMqq <mqqandkobe@163.com>
2019-03-17 23:21:29 +0800

    添加b test输出语句

--- one/src/com/mqq/demo1/ATest.java
+++ one/src/com/mqq/demo1/ATest.java
@@ -1,11 +1,12 @@
 package com.mqq.demo1;
 
-import com.sun.org.apache.xalan.internal.xsltc.compiler.sym;
+
 
 public class ATest {
 
     public static void main(String[] args) {
         System.out.println("a test");
+        System.out.println("b test");
     }
 
 }
