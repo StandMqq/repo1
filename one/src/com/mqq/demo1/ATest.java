10a748f5f7db7ea245d4471d85939a17ce69fa23
LegendaryMqq <mqqandkobe@163.com>
2019-03-17 17:14:38 +0800

    与repo1仓库建立联系

---
+++ one/src/com/mqq/demo1/ATest.java
@@ -0,0 +1,11 @@
+package com.mqq.demo1;
+
+import com.sun.org.apache.xalan.internal.xsltc.compiler.sym;
+
+public class ATest {
+
+    public static void main(String[] args) {
+        System.out.println("a test");
+    }
+
+}
